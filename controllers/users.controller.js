69a25f0913e0be711d6c16b4e18ddc071d98c247
Jesus Garcia <jesusgarhdez@hotmail.com>
2025-03-16 13:46:44 -0600

    User delete and update

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,6 +1,7 @@
 const db = require("../helpers/firestore");
 const userCollection = db.collection("User")
 
+// Returns all users
 module.exports.get_users = async (_, res) => {
     try {
         const userRef = await userCollection.get();
@@ -15,6 +16,7 @@ module.exports.get_users = async (_, res) => {
     }
 }
 
+// Returns a specific user by id
 module.exports.get_user = async (req, res) => {
     let userId = req.params.id;
 
@@ -31,10 +33,11 @@ module.exports.get_user = async (req, res) => {
     }
 }
 
+// Creates a new user
 module.exports.create_user = async (req, res) => {
     try {
         const userRef = userCollection.doc();
-        const resp = await userRef.set(req.body);
+        await userRef.set(req.body);
         res.status(200).send("User created successfully");
     } catch (error) {
         console.error("Error getting users: ", error);
@@ -42,10 +45,30 @@ module.exports.create_user = async (req, res) => {
     }
 }
 
+// Deletes a specific user
 module.exports.delete_user = async (req, res) => {
-    // To do: delete an existing user
+    let userId = req.params.id;
+
+    try {
+        await userCollection.doc(userId).delete();
+        res.status(200).send("User deleted successfully");
+    } catch (error) {
+        console.error("Error deleting user: ", error);
+        res.status(500).send("Internal server error");
+    }
 }
 
+// Updates a specific user
 module.exports.update_user = async (req, res) => {
-    // To do: update an existing user
+    let userId = req.params.id;
+    let updatedUser = req.body;
+
+    try {
+        const userRef = userCollection.doc(userId);
+        await userRef.update(updatedUser);
+        res.status(200).send("User updated successfully");
+    } catch (error) {
+        console.error("Error updating user: ", error);
+        res.status(500).send("Internal server error");
+    }
 }
